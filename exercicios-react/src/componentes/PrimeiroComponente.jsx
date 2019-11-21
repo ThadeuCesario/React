eb8ffc7485e5833519334059da81289ea978ed56
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2019-11-21 16:22:34 -0200

    Meu primeiro componente.

---
+++ exercicios-react/src/componentes/PrimeiroComponente.jsx
@@ -0,0 +1,23 @@
+/* Como vamos ter sintaxe JSX nesse arquivo, é fundamental termos o import do React.
+ */
+import React from 'react'; 
+
+/* Nesse primeiro momento, criaremos um componente baseado em função.
+ * Temos dois tipos de componentes:
+ * - Componentes baseados em função;
+ * - Componentes baseados em classe;
+ */
+
+function primeiro(){
+    return <h1>Primeiro Componente</h1>;
+}
+export default primeiro;
+
+/* Outra forma:
+Quando exportamos diretamente a função, podemos ter uma função anônima.
+
+export default function(){
+    return <h1>Primeiro Componente</h1>;
+}
+
+ */
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; //Responsável por lançar nossos componentes para a página.
+import PrimeiroComponente from './componentes/PrimeiroComponente.jsx';
 
 const elemento = document.getElementById("root");
+
+/* Abaixo utilizaremos nosso primeiro componente. Veja que realizei a chamada de nosso componente
+ * utilizando a tag de abertura e fechamaneto: <PrimeiroComponente></PrimeiroComponente> ;
+ * Mas poderia utilizar também uma tag sem corpo: <PrimeiroComponente />
+ */
+ReactDOM.render(
+        <div>
+            <PrimeiroComponente></PrimeiroComponente> 
+        </div>
+    ,elemento);
+
+
+// const jsx = <h1>Hello React!</h1>;
 // ReactDOM.render('Olá React!', elemento);
-ReactDOM.render(<h1>Hello React!</h1>, elemento);  //Código JSX
+// ReactDOM.render(
+//     /* JSX -> Código que parece HTML mas que no final será convertido para código Javascript.
+//      */
+//     <ul>
+//         <li>Thadeu</li>
+//         <li>Karina</li>
+//         <li>Guilherme</li>
+//         <li>Katharina</li>
+//     </ul>
+//     ,elemento);
