e171be7ce87796302a2df2112a49dea52f3686a5
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2019-11-21 17:00:01 -0200

    Utilizando Arrow Function

--- exercicios-react/src/componentes/PrimeiroComponente.jsx
+++ exercicios-react/src/componentes/PrimeiroComponente.jsx
@@ -8,15 +8,34 @@ import React from 'react';
  * - Componentes baseados em classe;
  */
 
-function primeiro(){
-    return <h1>Primeiro Componente</h1>;
-}
-export default primeiro;
 
-/* Outra forma:
+
+
+export default () => <h1>Primeiro Componente! (Arrow)</h1>;
+
+
+
+
+
+
+
+
+
+
+/* 
+Outra forma:
 Quando exportamos diretamente a função, podemos ter uma função anônima.
 
 export default function(){
     return <h1>Primeiro Componente</h1>;
 }
- */
+--------------------------------------------------------
+Outra forma:
+Podemos exportar a função após sua implementação, porém nesse caso temos que deixar explícito seu nome.
+
+function primeiro(){
+    return <h1>Primeiro Componente</h1>;
+}
+export default primeiro;
+
+*/
