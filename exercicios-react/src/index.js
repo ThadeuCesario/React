2f2cfed6ffa5480d540b1c672eeee93e01780618
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2019-11-21 14:09:46 -0200

    Hello React! - Código JSX

---
+++ exercicios-react/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; //Responsável por lançar nossos componentes para a página.
+
+const elemento = document.getElementById("root");
+// ReactDOM.render('Olá React!', elemento);
+ReactDOM.render(<h1>Hello React!</h1>, elemento);  //Código JSX
