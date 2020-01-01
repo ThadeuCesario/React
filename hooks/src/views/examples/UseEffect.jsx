0000d27be5fd34de2f588387f7e095b6feb737d7
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-01-01 00:01:03 -0300

    using useEffect coder

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 const UseEffect = (props) => {
+    const [number, setNumber] = useState(1);
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+
+            <input type="number" onChange={(event) => setNumber(event.target.value)}/>
         </div>
     )
 }
