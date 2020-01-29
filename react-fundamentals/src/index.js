3e36a11ab9c24eeb3b4754ec082405b974e2d887
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-01-29 20:55:10 -0300

    Props - React

--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PrimeiroComponente from './componentes/PrimeiroComponente';
 
 const elemento = document.getElementById('root');
-ReactDOM.render(<h1>Olá React</h1>, elemento);
+
+// const jsx = <h1>Olá React 2</h1>
+// ReactDOM.render(jsx, elemento);
+
+ReactDOM.render(
+    <div>
+        <PrimeiroComponente valor='Hello World!' aBcD={123} calculo={2**8}></PrimeiroComponente>
+    </div>
+    , elemento);
