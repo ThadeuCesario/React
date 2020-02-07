7cccf2d0d485bfa54feba916ef08e6d6a46a89c2
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-02-06 21:08:13 -0300

    Ajustando Bind

--- react-fundamentals/src/componentes/Contador.jsx
+++ react-fundamentals/src/componentes/Contador.jsx
@@ -8,12 +8,15 @@ export default class Contador extends React.Component{
      * Por que o react espera que o retorno de um componente funcional seja o jsx e no
      * caso de um componente baseado em classe chamará a função render().
      */
-    maisUm(){
-        this.props.numero++;
-    }
 
-    menosUm(){
-        this.props.numero--;
+   
+    
+    maisUm = () =>{
+        /* O 'this' pode variar de acordo com quem chama a função. Em uma função tradicional
+         * de javascript o 'this' não está associado ao local que essa função está, mas sim em
+         * quem está chamando essa função.
+         */
+        this.props.numero++;
     }
 
      render(){
@@ -23,8 +26,36 @@ export default class Contador extends React.Component{
                  Número : {this.props.numero}
              </div>
              <button onClick={this.maisUm}>Incrementar</button>
-             <button onClick={this.menosUm}>Decrementar</button>
+             <button>Decrementar</button>
          </div>
          )
      }
 }
+
+
+
+
+/* Solução 1: Bind
+ * Utilizando construtor e o bind do javascript para garantir que em qualquer contexto
+ * o this irá apontar para o objeto atual.
+ */
+// constructor(props){
+//     super(props);
+//     this.maisUm = this.maisUm.bind(this);
+// }
+
+
+/* Solução 2: Função Arrow - Click
+ * Utilizando arrow function, o this é trabalhado de forma diferente sendo que o this é forçado
+ * a estar associado ao local em que a função foi escrita. Está relacionado com o contexto léxico.
+ */
+// <button onClick={() => this.maisUm()}>Incrementar</button>
+
+
+/* Solução 3: Função Arrow
+ * maisUm = () => {
+ *    //this.props.numero++;
+ *    console.log(this); 
+ * }
+ * 
+ */
