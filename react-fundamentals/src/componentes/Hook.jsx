7c1bb665d9a0be0f72bd8ea2d531a4d03db17d0c
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-02-08 12:20:10 -0300

    React useEffect

--- react-fundamentals/src/componentes/Hook.jsx
+++ react-fundamentals/src/componentes/Hook.jsx
@@ -1,7 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default props => {
     const [contador, setContador] = useState(100); 
+    const [status, setStatus] = useState('Par'); 
+
+    useEffect(() => {
+        contador % 2 === 0 ? setStatus('Par') : setStatus('Impar');
+    })
+
     return(
         <div>
             <h1>
