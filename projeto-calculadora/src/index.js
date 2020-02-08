4ea641cef7fd627d2a2e2f4615de26c5bea26f3c
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-02-08 20:20:14 -0300

    Projeto React Calculadora

--- projeto-calculadora/src/index.js
+++ projeto-calculadora/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
+ReactDOM.render(
+    <React.Fragment>
+        <h1>Calculadora</h1>
+        <Calculator />
+    </React.Fragment>
+    ,document.getElementById('root')
+    );
 
 serviceWorker.unregister();
