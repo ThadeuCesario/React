e3b23cbe05bf61a5c150a830371425c7ec183dcb
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-02-08 20:31:41 -0300

    Projeto React Calculadora

--- projeto-calculadora/src/main/Calculator.jsx
+++ projeto-calculadora/src/main/Calculator.jsx
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import Button from '../components/Button'
+import Button from '../components/Button';
+import Display from '../components/Display';
 import './Calculator.css';
 
 export default class Calculator extends Component{
     render(){
         return(
           <div className="calculator">
+              <Display value={100}/>
               <Button label="AC"/>
               <Button label="/"/>
               <Button label="7"/>
