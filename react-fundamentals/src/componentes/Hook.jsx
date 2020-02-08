e375db68314fc54e4c52f4dac6a15ec100b0d37e
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-02-08 12:24:42 -0300

    React useState, useEffect

--- react-fundamentals/src/componentes/Hook.jsx
+++ react-fundamentals/src/componentes/Hook.jsx
@@ -1,6 +1,12 @@
 import React, {useState, useEffect} from 'react';
 
 export default props => {
+    /* Nome do primeiro elemento armazena o valor e o nome do segundo elemento é o da função que irá alterar esse valor.
+     * Exemplo: 
+     *    const [contador, setContador] = useState(100); 
+     *    contador : É o valor que iremos alterar; 
+     *    setContador: É a função responsável pela alteração do contator;
+     */
     const [contador, setContador] = useState(100); 
     const [status, setStatus] = useState('Par'); 
 
