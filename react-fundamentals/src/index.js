fef57261c4fa0fc1ab232be07d2dfaace2ed9c6f
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-03-17 22:59:40 -0300

    jsx

--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const elemento = document.getElementById('root');
-ReactDOM.render(<h1>Hello React!</h1>, elemento);
+const jsx = <div>olá react</div>;
+ReactDOM.render(jsx, elemento);
