8ad7cd53cdec22e970b7034966de29324a15dfbb
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-03-22 16:32:22 -0300

    Componentes

---
+++ react-fundamentals/src/Componentes/SegundoComponente.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+function segundo(){
+return <h1>{5 + 5}</h1>;
+}
+
+export default segundo;
--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeiroComponente from './Componentes/PrimeiroComponente'
+import SegundoComponente from './Componentes/SegundoComponente';
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
+    <React.Fragment>
         <PrimeiroComponente />
+        <SegundoComponente />
+    </React.Fragment>
     , elemento);
