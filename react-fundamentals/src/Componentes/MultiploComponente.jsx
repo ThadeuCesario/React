a7d2c82c5e08b02584d87d5034f0aa6fdb276534
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-03-22 18:23:28 -0300

    Múltiplos componentes

---
+++ react-fundamentals/src/Componentes/MultiploComponente.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ComponenteA = props => <h1>{props.infoA}</h1>;
+
+const ComponenteB = props => <h1>{props.infoB}</h1>;
+
+export {ComponenteA, ComponenteB};
--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -6,6 +6,7 @@ import ComponenteAnonimo from './Componentes/AnonComponente';
 import ComponenteArrow from './Componentes/ArrowComponente';
 import ComponenteParametro from './Componentes/ComponenteComParametro';
 import ComponenteParametroNumerico from './Componentes/ComponenteParametroNumerico';
+import {ComponenteA, ComponenteB} from './Componentes/MultiploComponente';
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
@@ -16,6 +17,8 @@ ReactDOM.render(
         <ComponenteArrow />
         <ComponenteParametro info="Mensagem por parâmetro"/>
         <ComponenteParametroNumerico a={5} b={6} />
+        <ComponenteA infoA="Informação do primeiro componente"/>
+        <ComponenteB infoB="Informação do segundo componente"/>
     </React.Fragment>
     , elemento);
 
