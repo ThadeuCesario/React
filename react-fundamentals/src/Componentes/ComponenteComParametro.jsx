b32e0d80d8ae813b158c980d354f351092fb5207
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-03-22 17:38:38 -0300

    Componentes Com Parâmetro

---
+++ react-fundamentals/src/Componentes/ComponenteComParametro.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default (props) =>
+    <div>
+        <h1>{props.info}</h1>
+    </div>
--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -4,6 +4,8 @@ import PrimeiroComponente from './Componentes/PrimeiroComponente'
 import SegundoComponente from './Componentes/SegundoComponente';
 import ComponenteAnonimo from './Componentes/AnonComponente';
 import ComponenteArrow from './Componentes/ArrowComponente';
+import ComponenteParametro from './Componentes/ComponenteComParametro';
+import ComponenteParametroNumerico from './Componentes/ComponenteParametroNumerico';
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
@@ -12,6 +14,8 @@ ReactDOM.render(
         <SegundoComponente />
         <ComponenteAnonimo />
         <ComponenteArrow />
+        <ComponenteParametro info="Mensagem por parâmetro"/>
+        <ComponenteParametroNumerico a={5} b={6} />
     </React.Fragment>
     , elemento);
 
