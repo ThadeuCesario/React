036816220df832b803c950648ad68da9b4679d5c
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-03-22 18:32:35 -0300

    Trabalhando com elementos adjacentes

--- react-fundamentals/src/Componentes/MultiElementos.jsx
+++ react-fundamentals/src/Componentes/MultiElementos.jsx
@@ -3,5 +3,5 @@ import React from 'react';
 export default props =>
     <React.Fragment>
         <h1>Multielementos 1</h1>
-        <h2>Multielementos 2</h2>
+        <h2>Multielementos 5</h2>
     </React.Fragment>
