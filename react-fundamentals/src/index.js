970698a4bd21f104a318cd24c863e69f95e991c5
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-03-22 16:43:26 -0300

    Utilizando função anônima

--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeiroComponente from './Componentes/PrimeiroComponente'
 import SegundoComponente from './Componentes/SegundoComponente';
+import ComponenteAnonimo from './Componentes/AnonComponente';
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
     <React.Fragment>
         <PrimeiroComponente />
         <SegundoComponente />
+        <ComponenteAnonimo />
     </React.Fragment>
     , elemento);
 
