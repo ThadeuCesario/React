f7e3d2f3d44a40f0ddfa84e100ba2c5e1139595a
thadeu.cesario@icloud.com <thadeu.cesario@icloud.com>
2020-03-29 21:45:07 -0300

    new update

--- react-fundamentals/src/Componentes/TerceiroComponente.jsx
+++ react-fundamentals/src/Componentes/TerceiroComponente.jsx
@@ -2,5 +2,4 @@ import React from 'react';
 
 export default function terceiroComponente(){
     return <h1>Testando meu terceiro componente!</h1>
-    
 }
--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import PrimeiroComponente from './Componentes/PrimeiroComponente'
 import SegundoComponente from './Componentes/SegundoComponente';
@@ -6,6 +6,7 @@ import TerceiroComponente from './Componentes/TerceiroComponente';
 import ComponenteAnonimo from './Componentes/AnonComponente';
 import ComponenteArrow from './Componentes/ArrowComponente';
 import ComponenteParametro from './Componentes/ComponenteComParametro';
+import ComponenteParametroDois from './Componentes/ComponenteComParametroDois';
 import ComponenteParametroNumerico from './Componentes/ComponenteParametroNumerico';
 import {ComponenteA, ComponenteB as NomeQualquer} from './Componentes/MultiploComponente';
 import MultiElementos from './Componentes/MultiElementos';
@@ -19,13 +20,14 @@ import ComponenteFuncionalDois from './Componentes/ComponenteComFuncaoDois';
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
-    <React.Fragment>
+    <Fragment>
         <PrimeiroComponente />
         <SegundoComponente />
         <TerceiroComponente />
         <ComponenteAnonimo />
         <ComponenteArrow />
         <ComponenteParametro info="Mensagem por parâmetro"/>
+        <ComponenteParametroDois nome="Componente Parametro Dois - Passando Nome"/>
         <ComponenteParametroNumerico a={5} b={6} />
         <ComponenteA infoA="Informação do primeiro componente"/>
         <NomeQualquer infoB="Informação do segundo componente"/>
@@ -44,6 +46,6 @@ ReactDOM.render(
         </ComponenteCalculadora>
         <ComponenteFucional />
         <ComponenteFuncionalDois />
-    </React.Fragment>
+    </Fragment>
     , elemento);
 
