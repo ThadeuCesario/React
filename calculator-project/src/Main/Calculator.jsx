76cea423d17e26cb32f8cb0978f7dba24882b0a3
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-02 23:14:42 -0300

    Logical functions - Calculator Project

--- calculator-project/src/Main/Calculator.jsx
+++ calculator-project/src/Main/Calculator.jsx
@@ -7,7 +7,17 @@ import './Calculator.css';
 import Button from "../Components/Button";
 import Display from "../Components/Display";
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component{
+
+    state = {...initialState};
 
     constructor(props) {
         super(props);
@@ -18,7 +28,7 @@ export default class Calculator extends Component{
     }
 
     clearMemory(){
-        console.log('limpar');
+        this.setState({...initialState});
     }
 
     setOperation(operation){
@@ -26,14 +36,33 @@ export default class Calculator extends Component{
     }
 
     addDigit(n){
-        console.log(n);
+        if(n === '.' && this.state.displayValue.includes('.')){
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
+
+        const currentValue = clearDisplay ? '' : this.state.displayValue;
+
+        const displayValue = currentValue + n;
+
+        this.setState({displayValue, clearDisplay: false});
+
+        if(n !== '.'){
+            const i = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[i] = newValue;
+            this.setState({values});
+            console.log(values);
+        }
     }
 
 
     render() {
         return(
             <div className={'calculator'}>
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button label={'AC'} click={this.clearMemory} triple={true}/>
                 <Button label={'/'}  click={this.setOperation} operation={true}/>
                 <Button label={'7'}  click={this.addDigit}/>
