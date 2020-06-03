bce77fbb2b2f3917df194fe441926a2e8321ed8d
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-02 21:44:14 -0300

    Calculator Component

---
+++ calculator-project/src/Main/Calculator.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+/* Import CSS */
+import './Calculator.css';
+
+export default class Calculator extends Component{
+    render() {
+        return(
+            <div className={'calculator'}>
+                    Teste
+            </div>
+        );
+    }
+}
