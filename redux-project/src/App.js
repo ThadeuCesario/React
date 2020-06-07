e02c14949164c931d6383ad33d0861126fe2b978
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-07 12:41:32 -0300

    redux

--- redux-project/src/App.js
+++ redux-project/src/App.js
@@ -3,13 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import Card from "./components/card/Card";
 
+
+
 function App() {
   return (
     <div className="App">
         <h1>React/Redux</h1>
         <div className={'linha'}>
-            <Card>
-
+            <Card title={'Card 1'} red>
+                A
+            </Card>
+            <Card title={'Card 1'} green>
+                A
+            </Card>
+        </div>
+        <div className={'linha'}>
+            <Card title={'Card 3'} blue>
+                X
+            </Card>
+            <Card title={'Card 4'} purple>
+                X
             </Card>
         </div>
     </div>
