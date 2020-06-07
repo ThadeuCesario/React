114e831359511e0d7963eb23f096462967a2c90d
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-07 13:52:54 -0300

    using just react without redux

--- redux-project/src/components/soma/Soma.jsx
+++ redux-project/src/components/soma/Soma.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import Card from "../card/Card";
 
 export default props => {
+
+    const {min, max} = props
+
     return(
         <Card title={'Soma dos Números'} blue>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>10</strong>
+                    <strong>{min + max}</strong>
                 </span>
             </div>
         </Card>
--- redux-project/src/components/sorteio/Sorteio.jsx
+++ redux-project/src/components/sorteio/Sorteio.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 import Card from "../card/Card";
 
 export default props => {
+
+    const {min, max} = props;
+    const aleatorio = parseInt(Math.random() * (max - min)) + min;
+
     return(
         <Card title={'Sorteio de um Número'} purple>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>8</strong>
+                    <strong>{aleatorio}</strong>
                 </span>
             </div>
         </Card>
