bcc6ea7285d4c34242889e7206063bd0fb8fad07
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-07 22:31:09 -0300

    Adding cont to types

--- redux-project/src/store/actions/ActionCreatorNumeros.js
+++ redux-project/src/store/actions/ActionCreatorNumeros.js
@@ -3,16 +3,19 @@
 * Lembre-se que o ActionCreator retorna uma nova action.
 *
 * */
+
+import {NUM_MAX_ALTERADO, NUM_MIN_ALTERADO} from './actionTypes';
+
 export function alterarNumeroMinimo(novoNumero){
     return{
-       type: 'NUM_MIN_ALTERADO',
+       type: NUM_MIN_ALTERADO,
        payload: novoNumero
     }
 }
 
 export function alterarNumeroMaximo(novoNumero){
     return{
-        type: 'NUM_MAX_ALTERADO',
+        type: NUM_MAX_ALTERADO,
         payload: novoNumero
     }
 }
--- redux-project/src/store/reducers/numerosReducer.js
+++ redux-project/src/store/reducers/numerosReducer.js
@@ -1,3 +1,6 @@
+
+import {NUM_MAX_ALTERADO, NUM_MIN_ALTERADO} from '../actions/actionTypes';
+
 const initialState = {
     min: 1,
     max: 10
@@ -5,13 +8,13 @@ const initialState = {
 
 export default function(state = initialState, action){
     switch (action.type) {
-        case 'NUM_MIN_ALTERADO':
+        case NUM_MIN_ALTERADO:
             return{
                 ...state,
                 min: action.payload
             }
 
-        case 'NUM_MAX_ALTERADO':
+        case NUM_MAX_ALTERADO:
             return{
                 ...state,
                 max: action.payload
