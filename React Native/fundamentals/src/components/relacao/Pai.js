d85d6c649f98736bba9e2fe56a2d55189e286257
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-07-17 18:44:26 -0300

    creating our own snippet

---
+++ React Native/fundamentals/src/components/relacao/Pai.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+import Style from '../estilo';
+
+export default props => {
+    return (
+        <Text style={Style.fontG}>
+
+        </Text>
+    )
+}
