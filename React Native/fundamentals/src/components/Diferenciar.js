ab1463a25270a7a258ea5ef3a9971e14ec284e62
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-07-17 15:41:38 -0300

    Plataform

--- React Native/fundamentals/src/components/Diferenciar.js
+++ React Native/fundamentals/src/components/Diferenciar.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import {Text, View, Button} from 'react-native';
+import {Text, View, Button, Platform} from 'react-native';
 import Style from './estilo';
 
 export default props => {
-    return(
-        <Text>Componente Basico</Text>
-    )
+    if(Platform.OS === "android"){
+        return(
+            <Text style={Style.fontG}>Android</Text>
+        )
+    }
+    else if(Platform.OS === "ios"){
+        return(
+            <Text style={Style.fontG}>iOS</Text>
+        )
+    }
+    else{
+        return(
+            <Text style={Style.fontG}>
+                OMNI (Objeto Mobile Não Identificado)! LoL
+            </Text>
+        )
+    }
+
 }
