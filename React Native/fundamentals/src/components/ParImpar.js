2d999ffa467f7325b92f1037fd580d632daf19d5
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-07-17 18:36:23 -0300

    Ternary render conditional

--- React Native/fundamentals/src/components/ParImpar.js
+++ React Native/fundamentals/src/components/ParImpar.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import Style from './estilo';
 
 export default props => { 
-    const {num = 0} = props
+    const {num = 0} = props;
 
+    return(
+        <View>
+            <Text>O resultado retornado é:</Text>
+            {
+                ((num % 2) === 0)
+                ? <Text style={Style.fontG}>Par</Text>
+                : <Text style={Style.fontG}>Ímpar</Text>
+            }
+        </View>
+    );
 
-    if((props.num % 2) === 0){
-        return(
-            <Text style={Style.fontG}>Par</Text>
-        );
-    }
-    else{
-        return(
-            <Text style={Style.fontG}>Ímpar</Text>
-        );
-    }
-    
 }
