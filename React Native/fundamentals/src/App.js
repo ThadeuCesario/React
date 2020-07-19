24d1a1da8294ca900cdd387455954172353803ab
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-07-19 18:06:15 -0300

    creating states - class components

--- React Native/fundamentals/src/App.js
+++ React Native/fundamentals/src/App.js
@@ -70,7 +70,7 @@ export default () => {
                 <FlexboxV3 />
                 <FlexboxV4 />
             */}
-            <Mega nome={'Thadeu'}/>
+            <Mega qtdeNumeros={7}/>
         </SafeAreaView>
     )
 }
--- React Native/fundamentals/src/components/Mega/Mega.js
+++ React Native/fundamentals/src/components/Mega/Mega.js
@@ -3,9 +3,18 @@ import {Text, View, Button} from 'react-native';
 import Estilo from '../estilo';
 
 export default class Mega extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            qtdeNumeros: props.qtdeNumeros,
+        }
+    }
+
     render(){
         return(
-        <Text style={Estilo.fontG}>{this.props.nome}</Text>
+        <Text style={Estilo.fontG}>{this.state.qtdeNumeros}</Text>
         )
     }
 }
