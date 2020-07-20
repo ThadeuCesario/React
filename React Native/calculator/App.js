8c1eb37dafea249eb7c1e436f9e494f9f594f5f7
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-07-19 21:11:29 -0300

    Calculator layout

--- React Native/calculator/App.js
+++ React Native/calculator/App.js
@@ -14,28 +14,39 @@ import Display from './src/components/Display';
 const App = props => {
   const [displayValue, setDisplayValue] = useState('0');
 
+  const addDigit = n => {
+    setDisplayValue(n);
+  }
+
+  const clearMemory = _ => {
+    setDisplayValue('0')
+  }
+
+  const setOperation = operation => {
+    console.warn(operation);
+  }
 
   return(
     <View style={styles.container}>
       <Display value={displayValue} />
       <View style={styles.buttons}>
-        <Button label={'AC'} />
-        <Button label={'/'} />
-        <Button label={'7'} />
-        <Button label={'8'} />
-        <Button label={'9'} />
-        <Button label={'*'} />
-        <Button label={'4'} />
-        <Button label={'5'} />
-        <Button label={'6'} />
-        <Button label={'-'} />
-        <Button label={'1'} />
-        <Button label={'2'} />
-        <Button label={'3'} />
-        <Button label={'+'} />
-        <Button label={'0'} />
-        <Button label={'.'} />
-        <Button label={'='} />
+        <Button label={'AC'} triple onClick={clearMemory}/>
+        <Button label={'/'} operation onClick={() => setOperation('/')}/>
+        <Button label={'7'} onClick={() => addDigit(7)}/>
+        <Button label={'8'} onClick={() => addDigit(8)}/>
+        <Button label={'9'} onClick={() => addDigit(9)}/>
+        <Button label={'*'} operation onClick={() => setOperation('*')}/>
+        <Button label={'4'} onClick={() => addDigit(4)}/>
+        <Button label={'5'} onClick={() => addDigit(5)}/>
+        <Button label={'6'} onClick={() => addDigit(6)}/>
+        <Button label={'-'} operation onClick={() => setOperation('-')}/>
+        <Button label={'1'} onClick={() => addDigit(1)}/>
+        <Button label={'2'} onClick={() => addDigit(2)}/>
+        <Button label={'3'} onClick={() => addDigit(3)}/>
+        <Button label={'+'} operation onClick={() => setOperation('+')}/>
+        <Button label={'0'} double onClick={() => addDigit(0)}/>
+        <Button label={'.'} onClick={() => addDigit('.')}/>
+        <Button label={'='} operation onClick={() => setOperation('=')}/>
       </View>
     </View>
   )
--- React Native/calculator/src/components/Button.js
+++ React Native/calculator/src/components/Button.js
@@ -18,13 +18,38 @@ const styles = StyleSheet.create({
         textAlign: "center",
         borderWidth: 1,
         borderColor: "#888",
+    },
+    operationButton: {
+        color: "#fff",
+        backgroundColor: "#fa8231",
+    },
+    buttonDouble: {
+        width: (Dimensions.get("window").width / 2),
+
+    },
+    buttonTriple: {
+        width: (Dimensions.get("window").width / 4) *3,
     }
 });
 
 export default props => {
-    return(
+    const stylesButton = [styles.button];
+
+    if(props.double){
+        stylesButton.push(styles.buttonDouble);
+    }
+
+    if(props.triple){
+        stylesButton.push(styles.buttonTriple);
+    }
+
+    if(props.operation){
+        stylesButton.push(styles.operationButton);
+    }
+
+    return( 
         <TouchableHighlight onPress={props.onClick}>
-            <Text style={styles.button}>{props.label}</Text>
+            <Text style={stylesButton}>{props.label}</Text>
         </TouchableHighlight>
     )
 }
