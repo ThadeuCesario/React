48f409a9618e3bd248ce0dbc795ba3f0e2e187ff
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-07-26 01:30:20 -0300

    Todo App project

--- todo-app/frontend/src/main/App.jsx
+++ todo-app/frontend/src/main/App.jsx
@@ -3,8 +3,14 @@ import 'modules/font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
 
+import Todo from '../todo/todo';
+import About from '../about/about';
+
 export default props => {
     return(
-        <h1 className="container">Teste</h1>
+        <h1 className="container">
+            <Todo />
+            <About />
+        </h1>
     );
 }
---
+++ todo-app/frontend/src/template/menu.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <nav className='navbar navbar-inverse bg-inverse'>
+            <div className='container'>
+                <div className='nav-header'>
+                    <a className='navbar-brand' href='#'>
+                        <i className='fa fa-calendar-check-o'></i> TodoApp
+                    </a>
+                </div>
+            </div>
+
+            <div id='navbar' className='navbar-collapse collapse'>
+                <ul className='nav navbar-nav'>
+                    <li><a href='#/todos'>Tarefas</a></li>
+                    <li><a href='#/about'>Sobre</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
