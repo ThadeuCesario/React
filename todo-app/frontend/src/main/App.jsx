3ed9408e00c77f40e5536f25331498808a439bf9
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-08-30 15:27:56 -0300

    FirstRocket - React Native Rocketseat

--- todo-app/frontend/src/main/App.jsx
+++ todo-app/frontend/src/main/App.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 
 import Todo from '../todo/todo';
 import About from '../about/about';
+import Menu from '../template/menu';
 
 export default props => {
     return(
--- todo-app/frontend/src/template/menu.jsx
+++ todo-app/frontend/src/template/menu.jsx
@@ -4,18 +4,18 @@ export default props => {
     return (
         <nav className='navbar navbar-inverse bg-inverse'>
             <div className='container'>
-                <div className='nav-header'>
+                <div className='navbar-header'>
                     <a className='navbar-brand' href='#'>
                         <i className='fa fa-calendar-check-o'></i> TodoApp
                     </a>
                 </div>
-            </div>
-
-            <div id='navbar' className='navbar-collapse collapse'>
-                <ul className='nav navbar-nav'>
-                    <li><a href='#/todos'>Tarefas</a></li>
-                    <li><a href='#/about'>Sobre</a></li>
-                </ul>
+                
+                <div id='navbar' className='navbar-collapse collapse'>
+                    <ul className='nav navbar-nav'>
+                        <li><a href='#/todos'>Tarefas</a></li>
+                        <li><a href='#/about'>Sobre</a></li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
