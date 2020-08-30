bb4dcf7e6789f95e42f5bf633fc2e83ee293291e
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-08-30 16:24:08 -0300

    First styles in react native.

--- React Native/firstRocketMobile/src/index.js
+++ React Native/firstRocketMobile/src/index.js
@@ -1,6 +1,36 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
-export default App = _ => {
-  return <View />;
+/**
+ * As tags dentro do React Native, não possuem valor semântico.
+ * Além disso, as tags dentro do React Native não possuem estilização própria, como é o caso
+ * da web.
+ *
+ * Todos os componentes possuem por padrão o display flex. No ReactNative nos não temos herança de
+ * estilos.
+ * 
+ * View: div, footer, header, main, aside, section
+ * Text: p, span, strong, h1, h2, h3.
+ */
+
+export default App = _ny => {
+  return(
+    <View style={styles.container}>
+        <Text style={styles.title}>Hello GoStack.</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159C1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
