a7d6dc8d2aad0d030e99b83b4566c4003e4cfd06
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-05 19:30:09 -0300

    Creating Flag to select the level

--- React Native/mines/src/components/Flag.js
+++ React Native/mines/src/components/Flag.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
 const Flag = (props) => {
+  const {bigger} = props;
   return (
     <View style={styles.Container}>
-      <View style={styles.FlagPole} />
-      <View style={styles.Flag} />
-      <View style={styles.Base1} />
-      <View style={styles.Base2} />
+      <View style={[styles.FlagPole, bigger ? styles.FlagPoleBigger : null]} />
+      <View style={[styles.Flag, bigger ? styles.FlagBigger : null]} />
+      <View style={[styles.Base1, bigger ? styles.Base1Bigger : null]} />
+      <View style={[styles.Base2, bigger ? styles.Base2Bigger : null]} />
     </View>
   );
 };
@@ -45,6 +46,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#222',
     marginLeft: 5,
     marginTop: 12,
+  },
+  FlagPoleBigger: {
+    height: 28,
+    width: 4,
+    marginLeft: 16,
+  },
+  FlagBigger: {
+    height: 10,
+    width: 14,
+    marginLeft: 3,
+  },
+  Base1Bigger: {
+    height: 4,
+    width: 12,
+    marginTop: 20,
+    marginLeft: 12,
+  },
+  Base2Bigger: {
+    height: 4,
+    width: 20,
+    marginLeft: 8,
+    marginTop: 24,
   },
 });
 
