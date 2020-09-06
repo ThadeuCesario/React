35f5f0c3fe728046806c1fe58469c5861fdf95ea
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-05 23:40:51 -0300

    Creating all necessary functions to play our minedbattle.

--- React Native/mines/src/functions.js
+++ React Native/mines/src/functions.js
@@ -48,4 +48,85 @@ const createMinedBoard = (rows, columns, minesAmount) => {
   return board;
 };
 
-export {createMinedBoard};
+/* Estamos clonando o board porque no react quando precisamos alterar um estado, não podemos alterar diretamente
+a referência daquele objeto. Devemos criar novos objetos para evoluir aquele estado.
+*/
+const cloneBoard = (board) => {
+  return board.map((rows) => {
+    return rows.map((field) => {
+      return {...field};
+    });
+  });
+};
+
+const getNeighbors = (board, row, column) => {
+  const neighbors = [];
+  const rows = [row - 1, row, row + 1];
+  const columns = [column - 1, column, column + 1];
+
+  /**
+   * Para essa lógica funcionar devemos atendar duas condições:
+   * 1 - Excluir o próprio nó que o usuário escolheu.
+   * 2 - Excluir todos os nós que poderão estar fora dos limites da matriz.
+   */
+
+  rows.forEach((r) => {
+    columns.forEach((c) => {
+      const different = r !== row || c !== column;
+      const validRow = r >= 0 && r < board.length;
+      const validColumn = c >= 0 && c < board[0].length;
+
+      if (different && validRow && validColumn) {
+        neighbors.push(board[r][c]);
+      }
+    });
+  });
+  return neighbors;
+};
+
+const safeNeighborhood = (board, row, column) => {
+  const safes = (result, neighbor) => result && !neighbor.mined;
+  return getNeighbors(board, row, column).reduce(safes, true);
+};
+
+const openField = (board, row, column) => {
+  const field = board[row][column];
+  if (!field.opened) {
+    field.opend = true;
+    if (field.mined) {
+      field.exploded = true;
+    } else if (safeNeighborhood(board, row, column)) {
+      getNeighbors(board, row, column).forEach((n) =>
+        openField(board, n.row, n.column),
+      );
+    } else {
+      const neighbors = getNeighbors(board, row, column);
+      field.nearMines = neighbors.filter((n) => n.mined).length;
+    }
+  }
+};
+
+const fields = (board) => [].concat(...board);
+
+const hadExplosion = (board) =>
+  fields(board).filter((field) => field.exploded).length > 0;
+
+const pendding = (field) =>
+  (field.mined && !field.flagged) || (!field.mined && !field.opened);
+
+//Se o array gerado for igual a zero, o usuário ganhou o jogo.
+const wonGame = (board) => fields(board).filter(pendding).length === 0;
+
+const showMines = (board) =>
+  fields(board)
+    .filter((field) => field.mined)
+    .forEach((field) => (field.opened = true));
+
+export {
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines,
+};
