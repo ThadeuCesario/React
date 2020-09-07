9fbefae67c6929f51c57be691a57c708a7c005b2
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-07 16:00:35 -0300

    Drawer navigation

--- React Native/navigation/src/routes/index.js
+++ React Native/navigation/src/routes/index.js
@@ -3,14 +3,15 @@ import {SafeAreaView, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import Stack from './Stack';
 import Tab from './Tab';
-
+import Drawer from './Drawer';
 
 export default props => {
     return (
         <SafeAreaView style={styles.Container}>
             <NavigationContainer>
                 {/* <Stack /> */}
-                <Tab />
+                {/* <Tab /> */}
+                <Drawer />
             </NavigationContainer>
         </SafeAreaView>
     );
