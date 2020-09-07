57fb32304a67d9b5b20d8ad432892889bd9ec923
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-06 22:19:47 -0300

    Testing react stack navigation

--- React Native/navigation/src/views/TelaA.js
+++ React Native/navigation/src/views/TelaA.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TextoCentral from '../components/TextoCentral';
 
 export default props => {
+    const {navigation} = props;
     return (
         <TextoCentral bgColor='#e53935'>
             Tela A
