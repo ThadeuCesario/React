ca6eac519b42e05f2ab1606ef1efe14b4b91676c
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-07 15:53:37 -0300

    Navigation using tabs

--- React Native/navigation/src/views/TelaC.js
+++ React Native/navigation/src/views/TelaC.js
@@ -2,10 +2,13 @@ import React from 'react';
 import TextoCentral from '../components/TextoCentral';
 
 export default props => {
-    const route = props.route || {params : {numero: 0}};
+    const r = props.route;
+    const numero = r
+    && r.params
+    && r.params.numero ? r.params.numero: 0;
     return (
         <TextoCentral bgColor='#9932cd'>
-            Tela C = {route.params.numero}
+            Tela C = {numero}
         </TextoCentral>
     )
 }
