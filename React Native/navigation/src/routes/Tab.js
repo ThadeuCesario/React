ab585a6da6234a6156601d17df9ff83154d48af2
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-07 17:39:15 -0300

    Inserting icons in our tabs

--- React Native/navigation/src/routes/Tab.js
+++ React Native/navigation/src/routes/Tab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import TelaA from '../views/TelaA';
 import TelaB from '../views/TelaB';
 import TelaC from '../views/TelaC';
@@ -8,16 +9,47 @@ const Tab = createBottomTabNavigator();
 
 export default props => {
     return (
-        <Tab.Navigator tabBarOptions={{
+        <Tab.Navigator 
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              switch(route.name) {
+                  case 'TelaA':
+                        iconName = focused
+                            ? 'ios-information-circle'
+                            : 'ios-information-circle-outline';
+                      break;
+
+                  case 'TelaB':
+                        iconName = focused
+                            ? 'ios-information-circle'
+                            : 'ios-information-circle-outline';
+                        break;
+
+                  case 'TelaC':
+                    iconName = focused
+                        ? 'ios-list-box'
+                        : 'ios-list';
+                      break;
+
+                  default:
+                      break;
+              }
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+        })}
+        tabBarOptions={{
             activeTintColor:'red',
             inactiveTintColor:'blue',
-            labelStyle: {
-                fontSize: 30,
-            }
+            // labelStyle: {
+            //     fontSize: 30,
+            // },
+            showLabel: true,
         }} initialRouteName="TelaB">
-            <Tab.Screen name='TelaA' component={TelaA}/> 
-            <Tab.Screen name='TelaB' component={TelaB}/> 
-            <Tab.Screen name='TelaC' component={TelaC}/>       
+            <Tab.Screen name='TelaA' component={TelaA} options={{title: 'Cadastro'}}/> 
+            <Tab.Screen name='TelaB' component={TelaB} options={{title: 'Produtos'}}/> 
+            <Tab.Screen name='TelaC' component={TelaC} options={{title: 'Listas'}}/>       
         </Tab.Navigator>
     );
 }
