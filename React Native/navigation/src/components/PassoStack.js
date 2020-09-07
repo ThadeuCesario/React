12653657fd9107efa9f0173af232f9e6ed7fcbc8
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-07 14:34:15 -0300

    Stack navigation

--- React Native/navigation/src/components/PassoStack.js
+++ React Native/navigation/src/components/PassoStack.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, Dimensions} from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
 
 export default props => {
-    const {children, avancar, navigation} = props;
+    const {children, avancar, navigation, voltar} = props;
 
     const styles = StyleSheet.create({
         Container: {
             flex: 1,
+        },
+        ContainerButton: {
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+        },
+        ButtonAction: {
+            width: windowWidth / 2,
         },
         BottomView: {
             flex: 1,
@@ -15,9 +25,14 @@ export default props => {
 
     return (
         <View style={styles.Container}>
-            <View>
+            <View style={styles.ContainerButton}>
                 {
-                    avancar ? <Button title={'Avançar'} onPress={() => {
+                    voltar ? <Button style={styles.ButtonAction} title={'Voltar'} onPress={() => {
+                        navigation.goBack()
+                    }}/> : false
+                }
+                {
+                    avancar ? <Button style={styles.ButtonAction} title={'Avançar'} onPress={() => {
                         navigation.navigate(avancar)
                     }}/> : false
                 }
