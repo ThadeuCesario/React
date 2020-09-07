2c7b27d899170eb658ab81f1744c28ee861cdb96
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-06 21:56:53 -0300

    routes

---
+++ React Native/navigation/src/routes/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export default props => {
+    return (
+        <SafeAreaView>
+            <Text>Arquivo principal de navegacao</Text>
+        </SafeAreaView>
+    );
+}
