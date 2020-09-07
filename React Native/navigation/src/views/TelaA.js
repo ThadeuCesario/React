d97e3ff5b7990592d1dff5afda5007699b7cbb80
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-06 21:49:13 -0300

    TelaA - navigation

---
+++ React Native/navigation/src/views/TelaA.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TextoCentral from '../components/TextoCentral';
+
+export default props => {
+    return (
+        <TextoCentral bgColor='#e53935'>
+            Tela A
+        </TextoCentral>
+    )
+}
