98c0e5a41e6595fdd00cd7013fb6a30b647f8e8c
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-07 14:11:58 -0300

    Simple header example to navigation

--- React Native/navigation/src/views/TelaA.js
+++ React Native/navigation/src/views/TelaA.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import TextoCentral from '../components/TextoCentral';
+import PassoStack from '../components/PassoStack';
 
 export default props => {
     const {navigation} = props;
     return (
-        <TextoCentral bgColor='#e53935'>
-            Tela A
-        </TextoCentral>
+        <PassoStack {...props} avancar={'TelaB'}>
+            <TextoCentral bgColor='#e53935'>
+                Tela A
+            </TextoCentral>
+        </PassoStack>
     )
 }
