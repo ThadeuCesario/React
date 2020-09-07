0ba93ec4a8f689a146814cf68561d77a2de043f7
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-07 14:22:15 -0300

    Stack navigation

--- React Native/navigation/src/routes/Stack.js
+++ React Native/navigation/src/routes/Stack.js
@@ -3,15 +3,34 @@ import {createStackNavigator} from '@react-navigation/stack';
 import TelaA from '../views/TelaA';
 import TelaB from '../views/TelaB';
 import TelaC from '../views/TelaC';
+import PassoStack from '../components/PassoStack';
 
 const Stack = createStackNavigator();
 
 export default props => {
     return (
-        <Stack.Navigator initialRouteName="TelaA">
-            <Stack.Screen name="TelaA" options={{title: 'Informações Iniciais'}}  component={TelaA}/>
-            <Stack.Screen name="TelaB" component={TelaB}/>
-            <Stack.Screen name="TelaC" component={TelaC}/>
+        <Stack.Navigator initialRouteName="TelaA" screenOptions={{headerShown: true}}>
+            <Stack.Screen name="TelaA" options={{title: 'Informações Iniciais'}}>
+                {props => (
+                    <PassoStack {...props} avancar={'TelaB'}>
+                        <TelaA />
+                    </PassoStack>
+                )}
+            </Stack.Screen>
+            <Stack.Screen name="TelaB">
+               {props => (
+                   <PassoStack {...props} avancar={'TelaC'}>
+                       <TelaB />
+                   </PassoStack>
+               )}
+            </Stack.Screen>
+            <Stack.Screen name="TelaC">
+                {props => (
+                   <PassoStack {...props} avancar={'TelaA'}>
+                       <TelaC />
+                   </PassoStack>
+               )}
+            </Stack.Screen>
         </Stack.Navigator>
     );
 }
