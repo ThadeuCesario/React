1301de579d938271def4e2306d7cfbf9d0e324ae
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-07 15:54:01 -0300

    Creating Tab navigation method

---
+++ React Native/navigation/src/routes/Tab.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import TelaA from '../views/TelaA';
+import TelaB from '../views/TelaB';
+import TelaC from '../views/TelaC';
+import PassoStack from '../components/PassoStack';
+
+const Tab = createBottomTabNavigator();
+
+export default props => {
+    return (
+        <Tab.Navigator tabBarOptions={{
+            activeTintColor:'red',
+            inactiveTintColor:'blue',
+            labelStyle: {
+                fontSize: 30,
+            }
+        }} initialRouteName="TelaB">
+            <Tab.Screen name='TelaA' component={TelaA}/> 
+            <Tab.Screen name='TelaB' component={TelaB}/> 
+            <Tab.Screen name='TelaC' component={TelaC}/>       
+        </Tab.Navigator>
+    );
+}
