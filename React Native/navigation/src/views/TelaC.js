97cb5236f9b2aeb1daffd7a4d4e602ec58fb1532
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-07 15:37:43 -0300

    TelaC stack navigation

--- React Native/navigation/src/views/TelaC.js
+++ React Native/navigation/src/views/TelaC.js
@@ -2,9 +2,10 @@ import React from 'react';
 import TextoCentral from '../components/TextoCentral';
 
 export default props => {
+    const route = props.route || {params : {numero: 0}};
     return (
         <TextoCentral bgColor='#9932cd'>
-            Tela C
+            Tela C = {route.params.numero}
         </TextoCentral>
     )
 }
