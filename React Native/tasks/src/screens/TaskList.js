ee3c8e4bb6429f909ddb9a2f1345ff626cf81b88
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-07 21:13:08 -0300

    Tasklist component

--- React Native/tasks/src/screens/TaskList.js
+++ React Native/tasks/src/screens/TaskList.js
@@ -1,12 +1,41 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+
+import todayImage from '../../assets/imgs/today.jpg';
 
 export default class TaskList extends Component {
-    render(){
-        return(
-            <View>
-                <Text>TaskList</Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.Container}>
+        <ImageBackground source={todayImage} style={styles.BackgroundImage} />
+        <View style={styles.TaskList}>
+          <Text>TaskList</Text>
+        </View>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1, //Permitindo que o componente cresça na tela inteira.
+  },
+
+  /**
+   * Veja que abaixo tenho um flex:3 para o BackgroundImage, e flex: 7 para o TaskList.
+   * Basicamente estou dizendo para o react native que basicamente estou dividindo minha tela e que o
+   * BackgroundImage ocupara 30% e o TaskList ocuparÁ 70%.
+   * Portanto lembre-se que o atributo flex não habilita o display flex. Ele basicamente informa para o
+   * elemento crescer, com uma determinada proporção.
+   * Veja eles estão crescendo em relação ao componentente pai, que possui flex: 1 e que está ocupando
+   * a tela inteira.
+   * Obs.: A propriedade flex e flexGrown, possuem o mesmo propósito.
+   */
+
+  BackgroundImage: {
+    flex: 3,
+  },
+  TaskList: {
+    flex: 7,
+  }
+});
