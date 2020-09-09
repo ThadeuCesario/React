18889b3c44429034d05d241d93db2d6f5bf75422
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-08 23:44:45 -0300

    Creating our main variables - props

--- React Native/tasks/src/components/Task.js
+++ React Native/tasks/src/components/Task.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-export default props => {
-    return(
-        <View>
-            <Text>Componente Task</Text>
-        </View>
-    )
-}
+export default (props) => {
+  const {desc, estimateAt, doneAt} = props;
+
+  return (
+    <View>
+      <Text>{desc}</Text>
+      <Text>{estimateAt}</Text>
+      <Text>{doneAt}</Text>
+    </View>
+  );
+};
