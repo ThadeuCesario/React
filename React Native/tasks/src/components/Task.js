e31b178649685ea6c915e8977114184f2c2cdd84
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-08 23:52:01 -0300

    import stylesheet - task component

--- React Native/tasks/src/components/Task.js
+++ React Native/tasks/src/components/Task.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default (props) => {
   const {desc, estimateAt, doneAt} = props;
