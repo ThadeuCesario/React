7e9d9bc61e8ccf1a9be35cb6b720c061407e783a
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-09-13 19:04:58 -0300

    Creating button to check

--- React Native/tasks/src/components/Task.js
+++ React Native/tasks/src/components/Task.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import commonStyles from '../commonStyles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const getCheckView = (_) => {
+  return (
+    <View>
+      <Text>opa</Text>
+    </View>
+  );
+};
 
 export default (props) => {
   const {desc, estimateAt, doneAt} = props;
 
   return (
     <View style={styles.Container}>
-      <Text>{desc}</Text>
-      <Text>{estimateAt + ''}</Text>
-      <Text>{doneAt + ''}</Text>
+      <View style={styles.CheckContainer}>{getCheckView()}</View>
+      <View>
+        <Text>{desc}</Text>
+        <Text>{estimateAt + ''}</Text>
+      </View>
     </View>
   );
 };
@@ -20,5 +32,8 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     alignItems: 'center', //cross axis
     paddingVertical: 10,
+  },
+  CheckContainer: {
+    width: '20%',
   },
 });
