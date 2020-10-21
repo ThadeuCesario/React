10f4922bfef315416d3d3d270d7325393b010683
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-21 00:57:37 -0300

    Mixpanel tracking data

--- React Native/Mixpanel/MixpanelIntegration/src/App.js
+++ React Native/Mixpanel/MixpanelIntegration/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, SafeAreaView, Button} from 'react-native';
+import Mixpanel from 'mixpanel-react-native';
 
 const App = _props => {
+    const initMixpanel = async () => {
+        const mixpanel = await Mixpanel.init('f5cdb449a8d5e57579010b7da2749d99');
+        mixpanel.optInTracking({'Name': 'Test'});
+        mixpanel.identify('12345');
+        mixpanel.track('TrackEvent', {'Status': 'Pending', 'Order': '123ABDC', 'Device':'Galaxy'});
+        mixpanel.people.set('Plan', 'AAA');
+    }
+
+    useState(() => {
+        initMixpanel();
+    }, []);
+
     return (
-        <SafeAreaView>
-            <Text>Thadeu Teste</Text>
+        <SafeAreaView style={{flex: 1, justifyContent: 'center'}}>
+            <View>
+                <Button title='Press'/>
+            </View>
         </SafeAreaView>
     )
 }
