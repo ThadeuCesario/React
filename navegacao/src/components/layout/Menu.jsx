cbd8549aa54196d20b3d022df56bcc2843814c22
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-19 22:23:03 -0300

    applying our first styles

--- navegacao/src/components/layout/Menu.jsx
+++ navegacao/src/components/layout/Menu.jsx
@@ -4,7 +4,12 @@ import './Menu.css';
 const Menu = props => {
     return (
         <aside className='Menu'>
-            Componente Menu
+            <nav>
+                <ul>
+                    <li><a href="/">Início</a></li>
+                    <li><a href="/">Sobre</a></li>
+                </ul>
+            </nav>
         </aside>
     );
 }
