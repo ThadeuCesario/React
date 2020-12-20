75847dc01b08288c7e406b44dd7ec023a4d56f5d
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-19 22:03:00 -0300

    implmenting examples

---
+++ navegacao/src/views/examples/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = props => {
+    return (
+        <div className="About">
+            <h1>Componente About</h1>
+            <p>O nosso sistema foi criado!</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ navegacao/src/views/examples/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = props => {
+    return (
+        <div className="Home">
+            <h1>Componente Home</h1>
+            <p>Bem vindo!</p>
+        </div>
+    );
+}
+
+export default Home;
