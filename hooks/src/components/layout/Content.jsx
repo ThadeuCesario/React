cfc80e003c13775641ebe1eb744908b59550ae26
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-21 23:12:29 -0300

    Learning about React Hooks

--- hooks/src/components/layout/Content.jsx
+++ hooks/src/components/layout/Content.jsx
@@ -5,10 +5,12 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../../views/examples/Home';
 import NotFound from '../../views/examples/NotFound';
 import UseState from '../../views/examples/UseState';
+import UseRef from '../../views/examples/UseRef';
 
 const Content = props => (
     <main className="Content">
         <Switch>
+            <Route path="/useRef" component={UseRef} />
             <Route path="/useState" component={UseState}/>
             <Route exact path="/" component={Home} />
             <Route path="*" component={NotFound} />
---
+++ hooks/src/views/examples/UseRef.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+
+const UseRef = props => (
+    <div className="UseRef">
+        <PageTitle
+            title="Módulo UseRef"
+            subtitle="UseRef são uma nova adição ao React 16.8." />
+    </div>
+)
+
+export default UseRef;
