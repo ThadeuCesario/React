ec0b8a1e10619ba38fb98ce2cd7cb96cbe1be2bb
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-21 23:25:44 -0300

    useState - count button

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
 
 const UseState = props => {
+    const [count, setCount] = useState(0);
+
     return (
         <div className="UseState">
             <PageTitle title="Hook UseState" subtitle="Estado em componentes funcionais!"/>
+
+            <SectionTitle title="Exercicio #01" />
+            <div className="center">
+                <span className="text">
+                    {count}
+                </span>
+                <div>
+                    <button className="btn" onClick={() => setCount(count - 1)}>-1</button>
+                    <button className="btn" onClick={() => setCount(count + 1)}>+1</button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercicio #02" />
         </div>
     )
 }
