8036a71a03eb02dfa6be7ec9cac833174918f1ec
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-24 19:08:57 -0300

    refactoring

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 const UseEffect = props => {
-    const [number, setNumber] = useState(1)
+    const [number, setNumber] = useState(1);
+    const [fatorial, setFatorial] = useState(1);
 
+    const findFactorial = useCallback(n => {
+        if(n === 0 || n === 1) return 1;
+        return n * findFactorial(n - 1);
+    }, []);
+
+    useEffect(() => {
+        setFatorial(findFactorial(number));
+    }, [number, findFactorial]);
 
     return (
         <div className="UseEffect">
@@ -11,13 +20,18 @@ const UseEffect = props => {
                 title="Módulo UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!" />
 
-            <input
-                className="input"
-                value={number}
-                onChange={event => setNumber(event.target.value)}
-                type="number"
-            />
-            <span className="text">{number}</span>
+            <div className="center">
+                <div>
+                    <span className="text">Fatorial: </span>
+                    <span className="text red">{fatorial}</span>
+                </div>
+                <input
+                    className="input"
+                    value={number}
+                    onChange={event => setNumber(event.target.value)}
+                    type="number"
+                />
+            </div>
         </div>
     )
 }
