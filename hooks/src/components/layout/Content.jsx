604b485e02fa4a857096a2711d029074e55fe100
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-26 16:58:50 -0300

    Learning about useMemo

--- hooks/src/components/layout/Content.jsx
+++ hooks/src/components/layout/Content.jsx
@@ -7,10 +7,12 @@ import NotFound from '../../views/examples/NotFound';
 import UseState from '../../views/examples/UseState';
 import UseRef from '../../views/examples/UseRef';
 import UseEffect from '../../views/examples/UseEffect';
+import UseMemo from '../../views/examples/UseMemo';
 
 const Content = props => (
     <main className="Content">
         <Switch>
+            <Route path="/useMemo" component={UseMemo} />
             <Route path="/useEffect" component={UseEffect} />
             <Route path="/useRef" component={UseRef} />
             <Route path="/useState" component={UseState}/>
---
+++ hooks/src/views/examples/UseMemo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+
+const UseMemo = props => {
+    return (
+        <div className="UseMemo">
+            <PageTitle
+                title="Hook UseMemo"
+                subtitle="Retorna um valor memorizado" />
+        </div>
+    )
+}
+
+export default UseMemo;
