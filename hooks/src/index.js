6bf33fab217cf16b9b6cbedc32dc0935c7aae713
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-29 22:03:51 -0300

    inserting semicolumns

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,8 +1,8 @@
-import './index.css'
-import ReactDOM from 'react-dom'
-import React from 'react'
+import './index.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
 
-import App from './views/App'
+import App from './views/App';
 
 ReactDOM.render(
     <App />,
