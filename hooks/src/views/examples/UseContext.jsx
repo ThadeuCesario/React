bbe217cb27a6b5c4f7382f57f842b0719a8ebf88
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-12-31 18:29:35 -0300

    Second example using contextAPI - implementing Store component

--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,6 +1,9 @@
-import React, {useContext} from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useContext, useEffect} from 'react';
 import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
 import DataContext from '../../data/DataContext';
+import {AppContext} from '../../data/Store';
 
 const UseContext = props => {
     const {state, setState} = useContext(DataContext);
@@ -12,12 +15,20 @@ const UseContext = props => {
         })
     }
 
+    const {number, text, setNumber, setText} = useContext(AppContext);
+
+    useEffect(() => {
+        if(number > 1250) setText('Eita!');
+        else setText('ContextAPI + Hooks');
+    }, [number]);
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!" />
 
+            <SectionTitle title="Exercícío #01" /> 
             <div className="center">
                 <span className="text">
                     {state.text}
@@ -29,6 +40,17 @@ const UseContext = props => {
                 <div>
                     <button className="btn" onClick={() => addNumber(-1)}>-1</button>
                     <button className="btn" onClick={() => addNumber(1)}>+1</button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercícío #02" />
+            <div className="center">
+                <span className="text">{text}</span>
+                <span className="text">{number}</span>
+
+                <div>
+                    <button className="btn" onClick={() => setNumber(number - 1)}>-1</button>
+                    <button className="btn" onClick={() => setNumber(number + 1)}>+1</button>
                 </div>
             </div>
         </div>
