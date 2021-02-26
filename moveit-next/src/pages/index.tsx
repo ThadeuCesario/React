c372a22dc954c619dac1ecd2ec934e165681435b
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2021-02-25 22:56:44 -0300

    creating countdown button

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -2,12 +2,15 @@ import CompletedChallanges from "../components/CompletedChallanges";
 import Countdown from "../components/Countdown";
 import ExpirienceBar from "../components/ExpirienceBar";
 import Profile from "../components/Profile";
-
+import Head from 'next/head';
 import styles from '../styles/Pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExpirienceBar />
 
       <section>
