d102efb82961eebfd837caddff2f1786c369901f
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2021-02-25 21:44:17 -0300

    Migrando projeto moveit para NextJS

---
+++ moveit-next/src/pages/_app.tsx
@@ -0,0 +1,11 @@
+/**
+ * Tudo que for repetir em todas as páginas, será inserido nesse arquivo.
+ */
+
+import '../styles/global.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp;
