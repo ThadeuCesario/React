6dfe2c81234b7d9f1d69cd45acd23825906f05ea
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2021-02-25 21:53:01 -0300

    aplicando _document no projeto nextjs - carregamento estatico

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 /**
  * Tudo que for repetir em todas as páginas, será inserido nesse arquivo.
  */
-
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp;
