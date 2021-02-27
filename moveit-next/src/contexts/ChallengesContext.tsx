4f6ec3c46dab4ae852fb6e44aefdf2edd27aad0e
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-02-27 19:13:56 -0300

    context moveit application

---
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -0,0 +1,6 @@
+import { create } from 'domain';
+import {createContext} from 'react';
+
+const ChallengesContext = createContext({});
+
+export {ChallengesContext};
--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -2,12 +2,13 @@
  * Tudo que for repetir em todas as páginas, será inserido nesse arquivo.
  */
 import '../styles/global.css';
+import {ChallengesContext} from '../contexts/ChallengesContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ChallengesContext.Provider>
       <Component {...pageProps} />
-    </>
+    </ChallengesContext.Provider>
   )
 }
 
