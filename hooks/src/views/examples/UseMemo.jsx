ecb4be6d8c5980501e2f816769f7eee7280598e4
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-04-09 11:47:31 -0300

    applying example with useMemo

--- hooks/src/views/examples/UseMemo.jsx
+++ hooks/src/views/examples/UseMemo.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, {useState, useMemo} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 const UseMemo = (props) => {
+    const [n1, setN1] = useState(0);
+    const [n2, setN2] = useState(0);
+    const [n3, setN3] = useState(0);
+
+    const sum = (a, b) => {
+        const future = Date.now() + 2000;
+        while(Date.now() < future) {}
+        return a + b;
+    }
+
+    const result = useMemo(() => sum(n1, n2), [n1, n2]);
+
     return (
         <div className="UseMemo">
             <PageTitle
                 title="Hook UseMemo"
                 subtitle="Retorna um valor memoizado!"
             />
+
+            <div className="center">
+                <input 
+                    type="number" 
+                    className="input" 
+                    value={n1} 
+                    onChange={(event) => setN1(parseInt(event.target.value))}    
+                />
+                <input 
+                    type="number" 
+                    className="input" 
+                    value={n2} 
+                    onChange={(event) => setN2(parseInt(event.target.value))}    
+                />
+                <input 
+                    type="number" 
+                    className="input" 
+                    value={n3} 
+                    onChange={(event) => setN3(parseInt(event.target.value))}    
+                />
+                <span className="text">{result}</span>
+            </div>
         </div>
     )
 }
